47ccf6bd71088ec9bd6a4c9db2e76fba913be2a5
Marchal <marchalscott@gmail.com>
2019-03-20 09:10:52 -0600

    Adding trail data.

--- graphql-server-practice-master/data/trail-data.js
+++ graphql-server-practice-master/data/trail-data.js
@@ -12,5 +12,26 @@ module.exports.traildata = [
             "stars": 4,
             "difficulty": "greenBlue",
             "length": 7.4
+        },
+        {
+            "name": "Betasso Preserve",
+            "type": "Featured Ride",
+            "stars": 4,
+            "difficulty": "greenBlue",
+            "length": 7.4
+        },
+        {
+            "name": "Betasso Preserve",
+            "type": "Featured Ride",
+            "stars": 4,
+            "difficulty": "greenBlue",
+            "length": 7.4
+        },
+        {
+            "name": "Betasso Preserve",
+            "type": "Featured Ride",
+            "stars": 4,
+            "difficulty": "greenBlue",
+            "length": 7.4
         }
     ]
