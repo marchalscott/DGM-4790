c32b66e8e578af3b451c10989487a19175ae08d7
Marchal <marchalscott@gmail.com>
2019-03-29 21:47:50 -0600

    added index.ts file with create trail data.

---
+++ graphql-server-practice-master/index.ts
@@ -0,0 +1,15 @@
+import { prisma } from './generated/prisma-client'
+
+// A `main` function so that we can use async/await
+async function main() {
+
+  // Create a new user called `Alice`
+  const newTrail = await prisma.createTrails({ name: 'Alice' })
+  console.log(`Created new user: ${newTrail.name} (ID: ${newTrail.id})`)
+
+  // Read all users from the database and print them to the console
+  const allUsers = await prisma.trailses()
+  console.log(allTrails)
+}
+
+main().catch(e => console.error(e))
