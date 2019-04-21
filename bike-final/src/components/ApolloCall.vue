fec5985775736707f70cc53fdf7247cb0aeeb338
Marchal <marchalscott@gmail.com>
2019-04-20 22:36:06 -0600

    added sample code for mongoDB

---
+++ bike-final/src/components/ApolloCall.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <v-btn color="info" v-on:click="getAllGraphql()">All</v-btn>
+	  {{myItem}}
+	  error: {{error}}
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+  data() {
+    return {
+		error: '',
+    	myItem: {}
+    };
+  },
+  methods: {
+    getAllGraphql: function() {
+     this.$apollo
+        .query({
+          query: gql`
+			[YOUR QUERY FROM THE PLAYGROUND GOES HERE]
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+	 	.catch(err => {
+		this.error = err; 
+	 })
+    }
+  }
+};
+</script>
+<style>
+</style>
